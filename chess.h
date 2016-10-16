d339a10ad16bc95f29f4c1c4abea0f94e196752b
NewdawnALM <NewdawnALM@github.com>
2016-10-16 23:32:52 +0800

    棋盘面板类声明

---
+++ chess.h
@@ -0,0 +1,43 @@
+#ifndef  __CCHESS_H
+#define  __CCHESS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+using std::ostream;
+
+enum DIRECT
+{
+    UP = 0,
+    DOWN,
+    LEFT,
+    RIGHT,
+    UNKOWN
+};
+
+class CChess
+{
+    friend ostream& operator << (ostream &, const CChess &);
+    static int iLimitNum;
+public:
+    CChess(const string &state, int row, int col);
+    CChess(const vector<vector<char>> &vec2);
+    bool operator < (const CChess &rhs) const;
+    bool operator == (const CChess &rhs) const;
+
+    void check_row_col() const ;
+    void check_value() const ;
+    void output(ostream &out, const string &colSpace=" ", const string &rowSpace="\n") const;
+
+    string strState;
+    int iRow, iCol;
+    int iSteps;
+    int iZeroIdx;
+    const CChess *pparent;      // 必须指向实实际际存在的值！注意不要指向一个局部变量等！
+    int iMoveFromLast;
+    static const string directs[5];
+};
+
+#endif
